0460ad7bc5a1889924b93fb75ad6b83fe202013e
Viktoriia Badisova <vbadisova@gmail.com>
2018-06-04 12:56:06 +0300

    implement drawing in DrawView

--- TouchTracker/View/DrawView.swift
+++ TouchTracker/View/DrawView.swift
@@ -11,4 +11,26 @@ import UIKit
 class DrawView: UIView {
     var currentLine: Line?
     var finishedLines = [Line]()
+    
+    func stroke(_ line: Line) {
+        let path = UIBezierPath()
+        path.lineWidth = 10
+        path.lineCapStyle = .round
+        
+        path.move(to: line.begin)
+        path.addLine(to: line.end)
+        path.stroke()
+    }
+    
+    override func draw(_ rect: CGRect) {
+        UIColor.black.setStroke()
+        for line in finishedLines {
+            stroke(line)
+        }
+        if let line = currentLine{
+            UIColor.red.setStroke()
+            stroke(line)
+        }
+    }
+    
 }
