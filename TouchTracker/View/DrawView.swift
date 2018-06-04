b9bed3b6c9ea98de1f82a8539f7b01d0035be0c8
Viktoriia Badisova <vbadisova@gmail.com>
2018-06-04 13:29:21 +0300

    implement interruption

--- TouchTracker/View/DrawView.swift
+++ TouchTracker/View/DrawView.swift
@@ -66,4 +66,10 @@ class DrawView: UIView {
         setNeedsDisplay()
     }
     
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print(#function)
+        currentLines.removeAll()
+        setNeedsDisplay()
+    }
+    
 }
