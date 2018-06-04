eef41ea078e35ccc9e16a23d12c246670e0060f2
Viktoriia Badisova <vbadisova@gmail.com>
2018-06-04 12:47:21 +0300

    skeleton project

---
+++ TouchTracker/Model/Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Виктория Бадисова on 04.06.2018.
+//  Copyright © 2018 Виктория Бадисова. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
---
+++ TouchTracker/View/DrawView.swift
@@ -0,0 +1,14 @@
+//
+//  DrawView.swift
+//  TouchTracker
+//
+//  Created by Виктория Бадисова on 04.06.2018.
+//  Copyright © 2018 Виктория Бадисова. All rights reserved.
+//
+
+import UIKit
+
+class DrawView: UIView {
+    var currentLine: Line?
+    var finishedLines = [Line]()
+}
