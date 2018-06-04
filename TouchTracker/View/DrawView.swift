2e84147e7c4957d7453f1a465231988dcbe111da
Viktoriia Badisova <vbadisova@gmail.com>
2018-06-04 13:06:35 +0300

    implement touching in DrawView

--- TouchTracker/View/DrawView.swift
+++ TouchTracker/View/DrawView.swift
@@ -33,4 +33,30 @@ class DrawView: UIView {
         }
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first!
+        let location = touch.location(in: self)
+        currentLine = Line(begin: location, end: location)
+        setNeedsDisplay()
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first!
+        let location = touch.location(in: self)
+        currentLine?.end = location
+        setNeedsDisplay()
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if var line = currentLine {
+            let touch = touches.first!
+            let location = touch.location(in: self)
+            line.end = location
+            finishedLines.append(line)
+        }
+        currentLine = nil
+        
+        setNeedsDisplay()
+    }
+    
 }
